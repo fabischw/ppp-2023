deeb975181339545fe23c9c81877fae98bb60ed0
not-too-deranged <135414938+not-too-deranged@users.noreply.github.com>
2023-11-24 10:55:41 +0100

    Aufgabe06 Jasmin 1778099, 1623571 (#140)

---
+++ Exercises/JasminSteidl/Übung6.py
@@ -0,0 +1,142 @@
+# You get a recording of a terminal session moving around a file system.
+# The filesystem consists of a tree of files (plain data) and directories (which can contain other directories or files). 
+# The outermost directory is called /. You can navigate around the filesystem, moving into or out of directories
+# and listing the contents of the directory you're currently in.
+#
+# Within the terminal output, lines that begin with $ are commands you executed, very much like some modern computers:
+#
+# cd means change directory. This changes which directory is the current directory, but the specific result depends on the argument:
+# cd x moves in one level: it looks in the current directory for the directory named x and makes it the current directory.
+# cd .. moves out one level: it finds the directory that contains the current directory, then makes that directory the current directory.
+# cd / switches the current directory to the outermost directory, /.
+# ls means list. It prints out all of the files and directories immediately contained by the current directory:
+# 123 abc means that the current directory contains a file named abc with size 123.
+# dir xyz means that the current directory contains a directory named xyz.
+#
+# Here's an example:
+#
+# $ cd /
+# $ ls
+# dir a
+# 14848514 b.txt
+# 8504156 c.dat
+# dir d
+# $ cd a
+# $ ls
+# dir e
+# 29116 f
+# 2557 g
+# 62596 h.lst
+# $ cd e
+# $ ls
+# 584 i
+# $ cd ..
+# $ cd ..
+# $ cd d
+# $ ls
+# 4060174 j
+# 8033020 d.log
+# 5626152 d.ext
+# 7214296 k
+#
+# Based on this example, you can deduce the directory structure must look something like this:
+# - / (dir)
+#   - a (dir)
+#     - e (dir)
+#       - i (file, size=584)
+#     - f (file, size=29116)
+#     - g (file, size=2557)
+#     - h.lst (file, size=62596)
+#   - b.txt (file, size=14848514)
+#   - c.dat (file, size=8504156)
+#   - d (dir)
+#     - j (file, size=4060174)
+#     - d.log (file, size=8033020)
+#     - d.ext (file, size=5626152)
+#     - k (file, size=7214296)
+#
+#
+# PART 1:
+# Write a function that reads a console session like the example above from a file, 
+# and then returns a dictionary listing the size of each directory. Directories should
+# be identified by their full path. For the example above:
+#
+# - The total size of directory `e` is 584 because it contains a single file `i` of size 584 and no other directories.
+# - The directory `a` has total size 94853 because it contains files
+#     `f` (size 29116), `g` (size 2557), and `h.lst` (size 62596),
+#     plus file `i` indirectly (`a` contains `e` which contains `i`).
+# - Directory `d` has total size 24933642.
+# - As the outermost directory, `/` contains every file. Its total size is 48381165.
+#
+# Based on that functions output, calculate the total size of all directories where the size of each individual
+# directory considered is at most 100000, and mention the result in your PR.
+# For the example above the two directories meeting the size requirement are `a` and `e`, while `/` and `d` are too large.
+# The sum of the size of a and e would be 95437.
+#
+#
+# PART 2:
+# In part 2 you need to identify a single directory to delete (including all sub-directories of course).
+# The total space available to the filesystem is 70000000, and you need to make enough room to fit a file of 
+# size 30000000.
+#
+# In the example above, the total size of the outermost directory (and thus the total amount of used space) is 48381165; 
+# this means that the size of the unused space must currently be 21618835, which isn't quite the 30000000 required.
+# Therefore, the update still requires a directory with total size of at least 8381165 to be deleted before it can run.
+#
+# To achieve this, you have the following options:
+#
+# Delete directory `e`, which would increase unused space by 584.
+# Delete directory `a`, which would increase unused space by 94853.
+# Delete directory `d`, which would increase unused space by 24933642.
+# Delete directory `/`, which would increase unused space by 48381165.
+# Directories `e` and `a` are both too small; deleting them would not free up enough space.
+# However, directories `d` and `/` are both big enough! 
+# Between these, choose the smallest: `d`, increasing unused space by 24933642.
+# 
+# Mention the total size of the deleted directory in the PR.
+# 
+# You can find the /actual/ input to both parts in data/terminal_record.txt
+###############################################################################
+with open('terminal_record.txt') as f:
+    task_input = f.read().splitlines()
+
+MAX_SIZE_FOR_ADDING = 100000
+MAX_SIZE_DIR = 70000000
+NEEDED_SPACE = 30000000
+
+class Dir:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.subdirs = {}
+        self.files = []
+
+
+root = Dir('/', None)
+current_dir = root
+
+for line in input[1:]:
+    line_parts = line.split(' ')
+    if line.startswith('dir'):
+        current_dir.subdirs[line_parts[1]] = Dir(line_parts[1], current_dir)
+    if line.startswith('$ cd'):
+        current_dir = current_dir.parent if line_parts[2] == '..' else current_dir.subdirs[line_parts[2]]
+    if line_parts[0].isnumeric():
+        current_dir.files.append(int(line_parts[0]))
+
+dir_sizes = []
+
+
+def calc_dir_sizes(dir):
+    file_size = sum(dir.files)
+    for subdir in dir.subdirs.values():
+        file_size += calc_dir_sizes(subdir)
+    dir_sizes.append(file_size)
+    return file_size
+
+
+calc_dir_sizes(root)
+print("The Sum of all the direcotries under 100000:", sum([size for size in dir_sizes if size < MAX_SIZE_FOR_ADDING]))
+
+freed_space = NEEDED_SPACE - (MAX_SIZE_DIR - dir_sizes[-1])
+print("Deleted file size:", min([size for size in dir_sizes if size >= freed_space]))
