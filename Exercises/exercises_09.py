b8346a30b64b9df1f50b17f17a3d28d3901b35d9
atrejut <atreju.tauschinsky@gmx.de>
2023-11-30 19:15:48 +0100

    Vorlesungsunterlagen 20231201

---
+++ Exercises/exercises_09.py
@@ -0,0 +1,18 @@
+# This is the continuation of last weeks exercise.
+# After having analysed the Titanic Dataset, you should now prepare a machine
+# learning model to predict whether passengers will survive.
+#
+# It is entirely up to you which algorithm and feature engineering to use.
+# I do recommend using some of the algorithms available in sklearn, but if you would 
+# like to use another library that's also ok. It's a good idea to try and evaluate different algorithms,
+# and different pre-processing/cleaning/feature-generation options if you have the time.
+#
+# I have split the training data into a train- and a test-set already. These can be found as separate files 
+# in the `data`-directory. You should only use the training set throughout your entire development -- feel free 
+# to use cross-validation or split the training set into a train- and a validation set again.
+# Once you have developed a final model, you should evaluate this model on the test set I've provided,
+# and report the MCC score for the test set in the title of your PR. You should **not** evaluate the test
+# set more than once for this initial submission!
+#
+# If you decide to change your code after code-review, you can report new values in the comments, but
+# leave the initial MCC in the title unchanged.
